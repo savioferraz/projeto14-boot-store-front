863dbfe373154cad4602c46263a63f6daae583f0
savioferraz <savio.ferraz88@gmail.com>
2022-09-14 20:32:25 -0300

    adicionado telas de cadastro e login

--- src/common/PrivatePage.js
+++ src/common/PrivatePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-const auth = JSON.parse(localStorage.getItem("mywallet"));
+const auth = JSON.parse(localStorage.getItem("bootstore"));
 
 export default function PrivatePage({ children }) {
   return auth?.token ? <Navigate to="/home" /> : children;
